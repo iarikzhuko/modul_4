92cefc658548373a457e39d7cc11c3647ea8556c
iarikzhuko <136339660+iarikzhuko@users.noreply.github.com>
2023-06-13 16:34:19 +0300

    Delete palindrom.py

--- palindrom.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(s):
-    A = [a.lower() for a in s if a.isalpha()]
-    if A==A[::-1]:
-        return True
-    else:
-        return False
-
-print(is_palindrome("мадам"))# для примера
- 
