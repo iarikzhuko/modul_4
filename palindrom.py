70bffe1da825e8e7a0e080c0eff4ec544a17a43e
iarikzhuko <136339660+iarikzhuko@users.noreply.github.com>
2023-06-13 17:33:53 +0300

    Add files via upload
    Я это реализовал через конструкцию if-else

---
+++ palindrom.py
@@ -0,0 +1,8 @@
+def is_palindrome(s):
+    A = [a.lower() for a in s if a.isalpha()]
+    if A==A[::-1]:
+       return True
+    else:
+       return False
+
+print(is_palindrome("мадам"))# для примера
