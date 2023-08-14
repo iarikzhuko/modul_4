a7bf1849936ac0a29b736cbdf2d25f924dd0c37d
iarikzhuko <136339660+iarikzhuko@users.noreply.github.com>
2023-08-14 11:21:48 +0300

    Add files via upload

--- app_lesson4/views.py
+++ app_lesson4/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Advertisment
 def index(request):
-    return render(request,'index.html')
+    advertisment=Advertisment.objects.all()
+    context={'advertisment': advertisment}
+    return render(request,'index.html', context)
 
 def top_sellers(request):
     return render(request,"top-sellers.html")
--- lesson4/settings.py
+++ lesson4/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS=[Path.joinpath(BASE_DIR,'Static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, "Media/")
+MEDIA_URL='Media/'
