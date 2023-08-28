5c03ac10275e53c9be87473c0e90896c16f50051
iarikzhuko <136339660+iarikzhuko@users.noreply.github.com>
2023-08-28 11:01:09 +0300

    Delete app_lesson4 directory

--- app_lesson4/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppLesson4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_lesson4'
-    verbose_name="Обьявленияc"
